1e2447df22af56bf1d1cb2ddb970e4cbaaacede3
Gustavo Coronel <gcoronelc@gmail.com>
2021-01-31 11:53:04 -0500

    Clase 1

---
+++ Clase01/SQLQuery1.sql
@@ -0,0 +1,25 @@
+
+
+USE EDUTEC;
+GO
+
+
+SELECT * FROM edutec.DBO.Curso;
+GO
+
+
+SELECT * FROM edutec.DBO.CursoProgramado;
+GO
+
+
+SELECT * FROM edutec.DBO.Matricula;
+GO
+
+
+select * from Northwind.dbo.Customers;
+go
+
+
+
+sp_helpdb edutec;
+go
