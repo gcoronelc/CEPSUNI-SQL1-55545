911f2d64c7a146da95eb1305ac3b055cf86bd0bf
Gustavo Coronel <gcoronelc@gmail.com>
2021-02-14 09:32:25 -0500

    Clase 03

--- Clase03/Ejemplo08.sql
+++ Clase03/Ejemplo08.sql
@@ -120,3 +120,112 @@ Instrucción INSERT en conflicto con la restricción CHECK 'CK_ARTICULO_PRECIO'.
 El conflicto ha aparecido en la base de datos 'DEMO01', tabla 'dbo.articulo'.
 Se terminó la instrucción.
 */
+
+
+
+drop table DEMO01.dbo.articulo;
+go
+
+CREATE TABLE DEMO01.dbo.articulo(
+	idArticulo INT NOT NULL, 
+	nombre VARCHAR(100) NOT NULL,
+	pre_costo MONEY NOT NULL,
+	pre_venta MONEY NOT NULL,
+	stock INT NOT NULL,
+	CONSTRAINT pk_articulo PRIMARY KEY(idArticulo),
+	CONSTRAINT u_articulo_nombre UNIQUE(nombre),
+	CONSTRAINT CUALQUIER_NOMBRE CHECK( PRE_VENTA > PRE_COSTO )
+)
+GO
+
+
+
+INSERT INTO DEMO01.dbo.articulo(idArticulo,nombre,pre_costo,pre_venta,stock)
+VALUES(1000, 'TELEVISOR', 2556.89, 1478.99, 100 );
+GO
+
+/*
+Msg 547, Level 16, State 0, Line 143
+Instrucción INSERT en conflicto con la restricción CHECK 'CUALQUIER_NOMBRE'. 
+El conflicto ha aparecido en la base de datos 'DEMO01', tabla 'dbo.articulo'.
+Se terminó la instrucción.
+*/
+
+
+
+drop table DEMO01.dbo.articulo;
+go
+
+CREATE TABLE DEMO01.dbo.articulo(
+	idArticulo INT NOT NULL, 
+	nombre VARCHAR(100) NOT NULL,
+	pre_costo MONEY NOT NULL,
+	pre_venta MONEY NOT NULL,
+	stock INT NOT NULL,
+	CONSTRAINT pk_articulo PRIMARY KEY(idArticulo),
+	CONSTRAINT u_articulo_nombre UNIQUE(nombre),
+	CONSTRAINT CK_ARTICULO_PRECIO CHECK( PRE_VENTA > PRE_COSTO )
+)
+GO
+
+
+INSERT INTO DEMO01.dbo.articulo(idArticulo,nombre,pre_costo,pre_venta,stock)
+VALUES(1000, 'TELEVISOR', 2556.89, 4478.99, 100 );
+GO
+
+select * from DEMO01.dbo.articulo;
+go
+
+ALTER TABLE DEMO01.dbo.articulo
+ADD porc_ganancia INT NOT NULL
+GO
+
+/*
+Msg 4901, Level 16, State 1, Line 179
+ALTER TABLE solo permite agregar columnas que contengan valores NULL, que tengan 
+la definición DEFAULT, que la columna que se agrega sea una columna de identidad 
+o de marca de tiempo, o si ninguna de las condiciones anteriores se cumplen, 
+la tabla debe estar vacía para que se pueda agregar esta columna. 
+La columna 'porc_ganancia' no se puede agregar a la tabla 'articulo' 
+no vacía porque no cumple estas condiciones.
+*/
+
+ALTER TABLE DEMO01.dbo.articulo
+ADD porc_ganancia INT NOT NULL
+CONSTRAINT df_articulo_porc_ganancia DEFAULT 0
+GO
+
+select * from DEMO01.dbo.articulo;
+go
+
+
+
+drop table DEMO01.dbo.articulo;
+go
+
+CREATE TABLE DEMO01.dbo.articulo(
+	idArticulo INT NOT NULL, 
+	nombre VARCHAR(100) NOT NULL,
+	pre_costo MONEY NOT NULL,
+	pre_venta MONEY NOT NULL,
+	stock INT NOT NULL,
+	CONSTRAINT pk_articulo PRIMARY KEY(idArticulo),
+	CONSTRAINT u_articulo_nombre UNIQUE(nombre),
+	CONSTRAINT CK_ARTICULO_PRECIO CHECK( PRE_VENTA > PRE_COSTO )
+)
+GO
+
+INSERT INTO DEMO01.dbo.articulo(idArticulo,nombre,pre_costo,pre_venta,stock)
+VALUES(1000, 'TELEVISOR', 2556.89, 4478.99, 100 );
+GO
+
+select * from DEMO01.dbo.articulo;
+go
+
+ALTER TABLE DEMO01.dbo.articulo
+ADD porc_ganancia INT NULL
+GO
+
+select * from DEMO01.dbo.articulo;
+go
+
